a99a1e14c54039d7f9bf2f076c8f87849dbec9d2
Ahmed Abdelfatah <ahmedabdelfatah@Ahmeds-MacBook-Pro-8.local>
2022-09-11 16:01:04 +0200

    initial commit

---
+++ Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Ahmed Abdelfatah on 04/09/2022.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message{
+    let sender: String //email of user
+    let body: String //message body
+}
